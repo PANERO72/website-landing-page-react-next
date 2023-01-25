39e2f379538ff084b0620514e0aa404ce49d6fd5
PANERO72 <panerootero@gmail.com>
2023-01-25 23:33:04 +0100

    Agregar varios archivos, 'assets' y correciones del código.

---
+++ components/logo.js
@@ -0,0 +1,22 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import { jsx, Image } from 'theme-ui';
+import { Link } from './link';
+
+export default function Logo({ src, ...rest }) {
+  return (
+    <Link
+      path="/"
+      sx={{
+        variant: 'links.logo',
+        display: 'flex',
+        cursor: 'pointer',
+        mr: 15,
+      }}
+      {...rest}
+    >
+      <Image src={src} alt="startup landing logo" />
+    </Link>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import Router from 'next/router';
+import { initGA, logPageView } from '/analytics/index';
+import 'react-multi-carousel/lib/styles.css';
+import 'react-modal-video/css/modal-video.min.css';
+import 'rc-drawer/assets/index.css';
+import 'typeface-dm-sans';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function CustomApp({ Component, pageProps }) {
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on('routeChangeComplete', logPageView);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
